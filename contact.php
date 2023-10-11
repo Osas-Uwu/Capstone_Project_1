1f15cc7746e803319939504df4a8d69ba0e439f4
Osariemen Uwuilekhue <106764970+Osas-Uwu@users.noreply.github.com>
2023-10-11 12:33:33 +0100

    My Capstone Project

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "jojobuter@gmail.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\n\n$message";
+
+    if (mail($to, $subject, $body)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error sending message.";
+    }
+}
+?>
